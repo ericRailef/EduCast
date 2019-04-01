f114b9a1a77a2e342c4391ad6e1aaae253cb5d57
ericRailef <earailef@uc.cl>
2019-04-01 01:12:25 -0300

    creación de rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from '../app/components/login/login.component';
+import { ListaCursosComponent } from './components/lista-cursos/lista-cursos.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'lista-cursos', component: ListaCursosComponent },
+  { path: 'login', component: LoginComponent},
+  { path: '**', pathMatch: 'full', redirectTo: 'login' }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {useHash:true})],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
